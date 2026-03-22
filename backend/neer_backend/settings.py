2f95c40e7b7952c417da3ed4df483455331ecf33
Ashutosh <mrashutheboy@gmail.com>
2026-03-22 07:56:58 +0530

    fix: read secrets from environment variables

--- backend/neer_backend/settings.py
+++ backend/neer_backend/settings.py
@@ -25,13 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9w35eidcq!+j831k+w%nx(y8_000r7x$y-u^@eewiw&je$@!vc'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-9w35eidcq!+j831k+w%nx(y8_000r7x$y-u^@eewiw&je$@!vc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
